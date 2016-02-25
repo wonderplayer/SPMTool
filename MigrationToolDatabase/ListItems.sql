07d580573980bc3fe7ad68cd9a9b0885194c610d
wonderplayer <roland.strakis@gmail.com>
2016-02-25 11:04:26 +0200

    Added indexes on table ListItems fields

--- MigrationToolDatabase/ListItems.sql
+++ MigrationToolDatabase/ListItems.sql
@@ -3,6 +3,13 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Value] NVARCHAR(MAX) NULL, 
     [FieldId] INT NOT NULL, 
-    [ListGUId] NVARCHAR(50) NOT NULL, 
-    CONSTRAINT [FK_ListItems_ListGUId_ToLists] FOREIGN KEY (ListGUId) REFERENCES Lists(GUId)
+    [ListGUId] NVARCHAR(50) NOT NULL 
 )
+
+GO
+
+CREATE INDEX [IX_ListItems_ListGUId] ON [dbo].[ListItems] (ListGUId)
+
+GO
+
+CREATE INDEX [IX_ListItems_FieldId] ON [dbo].[ListItems] (FieldId)
