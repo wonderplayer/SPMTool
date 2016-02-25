744ee382313dc10567424b1acb2472c1a475b159
wonderplayer <roland.strakis@gmail.com>
2016-02-25 10:54:59 +0200

    Added database

---
+++ MigrationToolDatabase/ListItems.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ListItems]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Value] NVARCHAR(MAX) NULL, 
+    [FieldId] INT NOT NULL, 
+    [ListGUId] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [FK_ListItems_ListGUId_ToLists] FOREIGN KEY (ListGUId) REFERENCES Lists(GUId)
+)
---
+++ MigrationToolDatabase/Lists.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[GUId] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NOT NULL, 
+    [InternalName] NCHAR(10) NOT NULL, 
+    [URL] NCHAR(10) NOT NULL, 
+    [DependencyId] NCHAR(10) NOT NULL
+)
